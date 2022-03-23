04b0919d634a00ad094cea5479e9911cddc0bf3b
Zeeshan3155 <31352812+Zeeshan3155@users.noreply.github.com>
2022-03-23 13:03:19 +0530

    Delete mini_dictionary.py

--- Assignment2/mini_dictionary.py
+++
@@ -1,8 +0,0 @@
-dict = {}
-strt_chr = input("Enter starting character for your ASCII value dictionary.\n")
-stop_chr = input("Enter last character for your ASCII value dictionary.\n")
-for i in range(ord(strt_chr),ord(stop_chr)+1):
-    a = chr(i)
-    b = ord(a)
-    dict[a] = b
-print(dict)
